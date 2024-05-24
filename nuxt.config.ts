96f1555685dce8642b7273ad38493a47be828968
Nicolas HOARAU <h.nico.sully@gmail.com>
2024-05-25 01:53:02 +0400

    installed vueuse for later use

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    ["@vexip-ui/nuxt", { prefix: "V" }],
-    "@nuxtjs/supabase",
-  ]
+  modules: [["@vexip-ui/nuxt", { prefix: "V" }], "@nuxtjs/supabase", "@vueuse/nuxt"]
 })
